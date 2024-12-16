7f1dd37065b9f7d8793779116389c21a14affe2b
Riley1800 <c20426892@mytudublin.ie>
2024-12-16 05:28:04 +0000

    Buttons almost fully set up, just need to fix prompt--Full implementation of steps and its design being the current work

--- ge1assignment/addons/kenney_prototype-kit/Models/GLB format/button1.gd
+++ ge1assignment/addons/kenney_prototype-kit/Models/GLB format/button1.gd
@@ -10,3 +10,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_button_variation_on_state_changed() -> void:
+	pass # Replace with function body.
